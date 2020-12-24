224752580899a78851820ffd66c094ebea394699
Vlad Smirnykh <vlad.ainur@gmail.com>
2020-12-24 16:30:26 +0300

    Add task of Day1 solution

--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -2,6 +2,29 @@ package day1;
 
 public class Task1 {
     public static void main(String[] args) {
+       int i = 0;
+       while (i < 10) {
+           System.out.print("JAVA" + " ");
+           i++;
+        }
 
+        System.out.println();
+
+       for (i = 0; i < 10; i++) {
+           System.out.print("JAVA" + " ");
+       }
+
+        System.out.println();
+
+        for (i = 0; i < 10; i++) {
+            System.out.println("JAVA");
+        }
+
+        int year = 1980;
+        int X = year;
+          while (X < 2020) {
+            System.out.println("Олимпиада " + X + " года");
+            X = X + 4;
+        }
     }
 }
