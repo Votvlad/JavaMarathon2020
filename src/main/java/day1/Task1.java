3213a82034977edb185f86f667ebe9fc5cb29e1c
Vlad Smirnykh <vlad.ainur@gmail.com>
2020-12-24 20:00:08 +0300

    Fix task of Day1 solution

--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -7,24 +7,5 @@ public class Task1 {
            System.out.print("JAVA" + " ");
            i++;
         }
-
-        System.out.println();
-
-       for (i = 0; i < 10; i++) {
-           System.out.print("JAVA" + " ");
-       }
-
-        System.out.println();
-
-        for (i = 0; i < 10; i++) {
-            System.out.println("JAVA");
-        }
-
-        int year = 1980;
-        int X = year;
-          while (X < 2020) {
-            System.out.println("Олимпиада " + X + " года");
-            X = X + 4;
-        }
     }
 }
--- src/main/java/day1/Task3.java
+++ src/main/java/day1/Task3.java
@@ -2,6 +2,9 @@ package day1;
 
 public class Task3 {
     public static void main(String[] args) {
-
+        int i = 0;
+        for (i = 0; i < 10; i++) {
+            System.out.println("JAVA");
+        }
     }
 }
