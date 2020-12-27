1aede14d96b84461e5d8a79c7d9aa55e4cae6a5e
Vlad Smirnykh <vlad.ainur@gmail.com>
2020-12-28 00:00:30 +0300

    Add task of Day3 solution

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,18 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        double x, y;
+        while (true) {
+            x = scanner.nextDouble();
+            y = scanner.nextDouble();
+            if (y == 0) {
+                break;
+            }
+            System.out.println(x / y);
+        }
     }
 }
