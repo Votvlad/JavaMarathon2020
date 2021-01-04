ddaa1dc31184572c92c8974b3332314003440875
Vlad Smirnykh <vlad.ainur@gmail.com>
2021-01-05 01:50:35 +0300

    Add task of Day5 solution

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,36 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+    Car car = new Car();
+    car.setYearOfIssue(1971);
+    car.setColorOfCar("white");
+    car.setModelOfCar("Dodge Challenger");
+    System.out.println("Year Of Issue: " + car.getYearOfIssue());
+    System.out.println("Color Of Car: " + car.getColorOfCar());
+    System.out.println("Model Of Car: " + car.getModelOfCar());
+    }
+}
+class Car {
+    private int YearOfIssue;
+    private String ColorOfCar;
+    private String ModelOfCar;
 
+    public void setYearOfIssue (int year) {
+        YearOfIssue = year;
+    }
+    public int getYearOfIssue () {
+        return YearOfIssue;
+    }
+    public void setColorOfCar (String color) {
+        ColorOfCar = color;
+    }
+    public String getColorOfCar () {
+        return ColorOfCar;
+    }
+    public void setModelOfCar (String model) {
+        ModelOfCar = model;
+    }
+    public String getModelOfCar () {
+        return ModelOfCar;
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,29 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+    Motorbike bike = new Motorbike(2013, "silver", "Harley-Davidson");
+    System.out.println("Year Of Issue: " + bike.getYear());
+    System.out.println("Color: " + bike.getColor());
+    System.out.println("Model: " + bike.getModel());
+    }
+}
+class Motorbike {
+    private  int year;
+    private  String color;
+    private  String model;
 
+    public Motorbike(int year, String color, String model) {
+        this.year = year;
+        this.color = color;
+        this.model = model;
+    }
+    public int getYear() {
+        return year;
+    }
+    public String getColor() {
+        return color;
+    }
+    public String getModel() {
+        return model;
     }
 }
